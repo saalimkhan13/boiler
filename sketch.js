296318819601395edfcb7d3e4ec54ba0e049e27d
Saalim Khan <68114437+saalimkhan13@users.noreply.github.com>
2020-09-13 04:40:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,54 +1,38 @@
-var fixedrect,movingrect;
-var go1,go2,go3,go4;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
+var engine, world;
+var ground, ball;
 
-function setup() {
-  createCanvas(1200,800);
- fixedrect=createSprite(600, 400, 50, 80);
- //fixedrect.shapeColor="blue";
- //fixedrect.debug=true;
- movingrect=createSprite(400,200,80,50);
- movingrect.shapeColor="lightBlue";
- movingrect.debug=true;
- 
- go1.createSprite(100,100,50,50);
- go2.createSprite(200,100,50,50);
- go3.createSprite(300,100,50,50);
- go4.createSprite(400,100,50,50);
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
 
- go1.shapeColor="red";
- go2.shapeColor="red";
- go3.shapeColor="red";
- go4.shapeColor="red";
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
 }
 
-function draw() {
-  background(255,255,255);  
-  movingrect.x=World.mouseX;
-  movingrect.y=World.mouseY;
-  console.log(movingrect.x-fixedrect.x);
-  
-if (isTouching(movingrect,g01)){
-  movingrect.shapeColor="green";
-  go1.shapeColor="green";
-}
-else {
-  movingrect.shapeColor="lightBlue";
-  go1.shapeColor="blue";
-}
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
 
-
-drawSprites();
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
 }
-
-function isTouching(object1,object2){
-  if(Object1.x-object2.x<object1.width/2+object2.width/2 &&                                                                        
-   object2.x-object1.x<object2.width/2+object1.width/2&&
-   object1.y-object2.y<Object1.height/2+object2.height/2 &&                                                                        
-   object2.y-object1.y<object2.height/2+object1.height/2){
-     return true;
- }
-  else {
-     return false;        
-  } 
-} 
